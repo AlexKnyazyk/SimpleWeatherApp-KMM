d203ecbbfee904e6931307ba6161bf57f8363c1b
Alex Knyazuk <alex.knyazuk@gmail.com>
2021-12-04 20:46:46 +0300

    Move location check to androidMain with actual class

--- common/src/commonMain/kotlin/com/simple/weather/app/data/repository/location/DeviceLocationRepositoryImpl.kt
+++ common/src/commonMain/kotlin/com/simple/weather/app/data/repository/location/DeviceLocationRepositoryImpl.kt
@@ -1,37 +1,5 @@
 package com.simple.weather.app.data.repository.location
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.location.Criteria
-import android.location.Location
-import android.location.LocationManager
 import com.simple.weather.app.domain.domain.repository.DeviceLocationRepository
-//import com.simple.weather.app.android.utils.isPermissionGranted
-import com.simple.weather.app.data.model.LocationResult
 
-//TODO
-internal class DeviceLocationRepositoryImpl(
-//    private val context: Context
-) : DeviceLocationRepository {
-
-    override fun getLocation(): LocationResult {
-//        return if (context.isPermissionGranted(Manifest.permission.ACCESS_COARSE_LOCATION)) {
-//            getLastKnownLocation()?.let { location ->
-//                LocationResult.Success(location.latitude, location.longitude)
-//            } ?: LocationResult.NoLocation
-//        } else {
-//            LocationResult.NoPermission
-//        }
-        return LocationResult.NoLocation
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getLastKnownLocation(): Location? {
-        return null
-//        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as? LocationManager
-//        val criteria = Criteria()
-//        return locationManager?.getBestProvider(criteria, false)?.let { bestProvider ->
-//            locationManager.getLastKnownLocation(bestProvider)
-//        }
-    }
-}
+internal expect class DeviceLocationRepositoryImpl : DeviceLocationRepository
--- common/src/iosMain/kotlin/com/simple/weather/app/di/CommonExpectActualComponent.kt
+++ common/src/iosMain/kotlin/com/simple/weather/app/di/CommonExpectActualComponent.kt
@@ -2,11 +2,13 @@ package com.simple.weather.app.di
 
 import com.simple.weather.app.data.PreferencesManager
 import com.simple.weather.app.data.db.AppDatabase
+import com.simple.weather.app.data.repository.location.DeviceLocationRepositoryImpl
+import com.simple.weather.app.domain.domain.repository.DeviceLocationRepository
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
-import io.ktor.client.engine.*
-import io.ktor.client.engine.ios.*
-import io.ktor.client.features.logging.*
+import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.engine.ios.Ios
+import io.ktor.client.features.logging.Logger
 import org.koin.dsl.module
 import platform.Foundation.NSLog
 
@@ -14,6 +16,7 @@ internal actual val commonExpectActualModule = module {
     factory { PreferencesManager() }
     factory { HttpClientEngineFactoryProvider() }
     factory { SqliteDriverFactory() }
+    single<DeviceLocationRepository> { DeviceLocationRepositoryImpl() }
 }
 
 internal actual class HttpClientEngineFactoryProvider {
