bfa3a97e539d4254d811fb59a298251e4c7a4a74
Alex Knyazuk <alex.knyazuk@gmail.com>
2022-03-17 12:42:40 +0400

    Update gradle

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization") version "1.6.10"
@@ -10,30 +8,30 @@ plugins {
 kotlin {
     android()
 
-    val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget = when {
-        System.getenv("SDK_NAME")?.startsWith("iphoneos") == true -> ::iosArm64
-        else -> ::iosX64
-    }
-
-    iosTarget("ios") {
-        binaries {
-            framework {
-                baseName = "common"
-            }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "common"
         }
     }
 
     sourceSets {
-        val ktorVersion = "1.6.3"
-        val coroutinesVersion = "1.5.2"
+        val ktorVersion = "1.6.7"
+        val coroutinesVersion = "1.6.0"
         val sqldelightVersion = "1.5.3"
-        val koinVersion = "3.1.4"
+        val koinVersion = "3.2.0-beta-1"
         val commonMain by getting {
             dependencies {
+                implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
+
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
                 implementation("io.ktor:ktor-client-serialization:$ktorVersion")
                 implementation("io.ktor:ktor-client-logging:$ktorVersion")
+
                 implementation("com.squareup.sqldelight:runtime:$sqldelightVersion")
                 implementation("com.squareup.sqldelight:coroutines-extensions:$sqldelightVersion")
                 implementation("io.insert-koin:koin-core:$koinVersion")
@@ -53,13 +51,17 @@ kotlin {
                 implementation("io.insert-koin:koin-android:$koinVersion")
             }
         }
-        val androidTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit"))
-                implementation("junit:junit:4.13.2")
-            }
-        }
-        val iosMain by getting {
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+
             dependencies {
                 implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}-native-mt"){
                     version {
@@ -70,12 +72,11 @@ kotlin {
                 implementation("com.squareup.sqldelight:native-driver:$sqldelightVersion")
             }
         }
-        val iosTest by getting
     }
 }
 
 android {
-    compileSdk = 30
+    compileSdk = 31
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
         minSdk = 21
